fc8cab7efa29440d12820d818b8124af1504754e
Savva Mishin <irafforty@yandex.ru>
2022-10-31 13:49:35 +0500

    test fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new UnauthorizedError401('Требуется авторизация'));
+    next(new UnauthorizedError401('Требуется авторизация.'));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -16,7 +16,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    next(new UnauthorizedError401('У вас нет доступа'));
+    next(new UnauthorizedError401('У вас нет доступа.'));
   }
   req.user = payload;
   next();
